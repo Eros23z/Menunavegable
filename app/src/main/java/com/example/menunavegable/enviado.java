c3651e5bdd36dbf6f2fb23326efe916863b861f8
EnerG <energycs23@live.com>
2019-09-19 23:48:14 -0300

    Navigation

--- app/src/main/java/com/example/menunavegable/enviado.java
+++ app/src/main/java/com/example/menunavegable/enviado.java
@@ -8,14 +8,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link enviado.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link enviado#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class enviado extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
